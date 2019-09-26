8128927ab4eb553321238185b796c865664c1847
Nicolas <nicolas.charvoz@epitech.eu>
2019-09-26 12:17:59 +0200

    dark mode handling

--- Source/DateTimePicker.swift
+++ Source/DateTimePicker.swift
@@ -694,6 +694,8 @@ extension DateTimePicker: UITableViewDataSource, UITableViewDelegate {
         cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         cell.textLabel?.textColor = darkColor.withAlphaComponent(0.4)
         cell.textLabel?.highlightedTextColor = highlightColor
+        cell.backgroundColor = .white
+
         // add module operation to set value same
         if tableView == amPmTableView {
             cell.textLabel?.text = (indexPath.row == 0) ? "AM" : "PM"
